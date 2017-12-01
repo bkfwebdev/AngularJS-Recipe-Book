a5e03ee6131aa669485391f23ff9564a4b8bbff2
bkfwebdev <bryant.feld@gmail.com>
2017-11-30 22:27:24 -0500

    works but wonky/glitchy

--- public/app.js
+++ public/app.js
@@ -2,12 +2,14 @@ var app = angular.module("app",["ngRoute"]);
 
 app.service("dataService", function($scope,$http){
 let baseURL = "http://localhost:5000";
-this.getAllRecipes = function(callback){$http.get(baseURL + "/api/recipes").then(callback);}
+this.getAllRecipes = function(callback){ $http.get(baseURL + "/api/recipes").then(callback);}
 this.getAllCategories = function(callback){$http.get(baseURL + "/api/categories").then(callback);}
 this.getAllFoodItems = function(callback){$http.get(baseURL + "/api/fooditems").then(callback);}
 this.getReciCat = function(targetCat,callback){$http.get(baseURL + "/api/recipes?category={" + targetCat + "}").then(callback)};
 this.getReciID = function(recipeID,callback){$http.get(baseURL + "/api/recipes/{" + recipeID + "}").then(callback)};
-this.update = function(recipeUpdate){}
-this.add = function(recipeAddition){}
-this.delete = function(deletedItem){}
+this.update = function(recipeUpdate){$http.put(baseURL + "/api/recipes/{" + recipeUpdate + "}");}
+this.add = function(recipeAddition){$http.post(baseURL + "/api/recipes/{" + recipeAddition + "}");}
+this.delete = function(deletedItem){$http.delete(baseURL + "/api/recipes/{" + deletedItem + "}");}
 });
+
+app.controller("RecipesController",function($scope){});
